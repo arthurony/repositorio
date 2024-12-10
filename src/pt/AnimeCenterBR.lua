0e580e27e6797c1be9865cc99d14d0dc3df36fa1
arthurony <49954136+arthurony@users.noreply.github.com>
2024-12-10 20:14:35 -0300

    Update AnimeCenterBR.lua

--- src/pt/AnimeCenterBR.lua
+++ src/pt/AnimeCenterBR.lua
@@ -14,8 +14,8 @@ local function expandURL(url)
 end
 
 return {
-    id = 12345,
-    name = "Anime Center BR",
+    id = 93083,
+    name = "AnimeCenterBR",
     baseURL = baseURL,
     imageURL = "https://raw.githubusercontent.com/arthurony/repositorio/refs/heads/main/icons/animecenterbr.png",
     hasSearch = false,
@@ -58,14 +58,20 @@ return {
             local chapters = { parseChapters(doc) }
             chapters = flatten(chapters)
 
+            -- Ordenar capítulos
+            table.sort(chapters, function(a, b)
+                local aNumber = tonumber(a.title:match("%d+")) or 0
+                local bNumber = tonumber(b.title:match("%d+")) or 0
+                return aNumber < bNumber
+            end)
+
             local o = 1
-            for i = #chapters, 1, -1 do
+            for i = 1, #chapters do
                 chapters[i]:setOrder(o)
                 o = o + 1
             end
 
             local chaptersList = AsList(chapters)
-            Reverse(chaptersList)
             info:setChapters(chaptersList)
         end
 
